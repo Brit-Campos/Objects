35234d897ccdb70c8f0d2d2b82474041fd0de684
Brit <Brit@DESKTOP-BFORP22>
2022-06-26 12:00:56 -0700

    Add project files.

---
+++ Objects/Program.cs
@@ -0,0 +1,34 @@
+﻿class Objects
+{
+    static void Main(string[] args)
+    {
+        // object = An instance of a class
+        //          A class can be used as a blueprint to create objects (OOP)
+        //          Objects can have fields & methods (characteristics & actions)
+
+        Human human1 = new Human();
+
+        human1.name = "Rick";
+        human1.age = 65;
+
+        human1.Eat();
+        human1.Sleep();
+
+        Console.ReadKey();
+    }
+}
+
+class Human
+{
+    public String name;
+    public int age;
+
+    public void Eat()
+    {
+        Console.WriteLine(name + " is eating.");
+    }
+    public void Sleep()
+    {
+        Console.WriteLine(name + " is sleeping.");
+    }
+}
